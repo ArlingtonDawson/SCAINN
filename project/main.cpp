6ea9baa5f5de8b91f199ebd537127098f5407cfb
Arlington Dawson <arlington.dawson@gmail.com>
2017-09-06 22:59:19 -0400

    Added some data prasing + git push test

--- project/main.cpp
+++ project/main.cpp
@@ -10,7 +10,13 @@ const char* kReplayFolder = "C:/Projects/SCAINN/project/maps/Replay/";
 
 class ReplayLearning : public sc2::ReplayObserver {
 public:
+	//Vectors for counting units seen/built/losses/kills
+	//the index will be off unittype id.
+	//Currently we are instrested in our units built, bad units seen, our units destroyed and bad guys destroyed.
 	std::vector<uint32_t> count_units_built_;
+	std::vector<uint32_t> count_units_seen_;
+	std::vector<uint32_t> count_myunits_destoryed_;
+	std::vector<uint32_t> count_otherunits_destoryed_;
 
 	ReplayLearning() :
 		sc2::ReplayObserver() {
@@ -18,18 +24,63 @@ public:
 
 	void OnGameStart() final {
 		const sc2::ObservationInterface* obs = Observation();
-		assert(obs->GetUnitTypeData().size() > 0);
-		count_units_built_.resize(obs->GetUnitTypeData().size());
-		std::fill(count_units_built_.begin(), count_units_built_.end(), 0);
+
+		//Lets get the unit type count. We will index the vectors based on unit type id.
+		const std::size_t unittypecount = obs->GetUnitTypeData().size();
+
+
+		//prepare each vector to be sized and filled with 0.
+		preparevector(unittypecount, count_units_built_);
+		preparevector(unittypecount, count_units_seen_);
+		preparevector(unittypecount, count_myunits_destoryed_);
+		preparevector(unittypecount, count_otherunits_destoryed_);
+
+
+	}
+
+	//simple function to resize and fill a vector with 0s.
+	void preparevector(std::size_t unittypecount, std::vector<uint32_t>& invector)
+	{
+		invector.resize(unittypecount);
+		std::fill(invector.begin(), invector.end(), 0);
+	}
+
+	//simple function to increment an unittype in our vectors.
+	void additemtovector(sc2::Unit unit, std::vector<uint32_t>& invector)
+	{
+		//Check to see if we are with in the bounds of the vector.
+		assert(uint32_t(unit.unit_type) < invector.size());
+		++invector[unit.unit_type];
 	}
 
 	void OnUnitCreated(const sc2::Unit& unit) final {
+		additemtovector(unit, count_units_built_);
 	}
+
+	void OnUnitEnterVision(const sc2::Unit& unit) final {
+		additemtovector(unit, count_units_seen_);
+	}
+
+	void OnUnitDestroyed(const sc2::Unit& unit)
+	{
+		//if the owner is 1 should be the player. More investigation on this
+		if (unit.owner == 1)
+		{
+			additemtovector(unit, count_myunits_destoryed_);
+		}
+		else
+		{
+			additemtovector(unit, count_otherunits_destoryed_);
+		}
+
+	}
+
 
 	void OnStep() final {
 		uint32_t game_loop = Observation()->GetGameLoop();
 
 		const sc2::ObservationInterface* obs = Observation();
+		//Grab our Score Object so that we can parse out some information. 
 		const sc2::Score& current_score = obs->GetScore();
 
 		std::cout << "Minerals: " << obs->GetMinerals() << " Gas: " << obs->GetVespene() << std::endl;
@@ -41,6 +92,35 @@ public:
 	}
 
 	void OnGameEnd() final {
+
+		const sc2::ObservationInterface* obs = Observation();
+		const sc2::UnitTypes& unit_types = obs->GetUnitTypeData();
+
+		//Display a vectors contents then wait for input.
+		displayvector(unit_types, count_units_built_, "Units Created:");
+		system("pause");
+		displayvector(unit_types, count_units_seen_, "Units Seen:");
+		system("pause");
+		displayvector(unit_types, count_myunits_destoryed_, "Units Destoried:");
+		system("pause");
+		displayvector(unit_types, count_otherunits_destoryed_, "Units Killed:");
+		system("pause");
+
+		std::cout << "Finished" << std::endl;
+	}
+
+	//Displays our vector information to our console screen.
+	void displayvector(const sc2::UnitTypes & unit_types, std::vector<uint32_t> invector, std::string label)
+	{
+		std::cout << label << std::endl;
+		for (uint32_t i = 0; i < invector.size(); ++i) {
+			//Ignore anything with 0s
+			if (invector[i] == 0) {
+				continue;
+			}
+
+			std::cout << unit_types[i].name << ": " << std::to_string(invector[i]) << std::endl;
+		}
 	}
 };
 
